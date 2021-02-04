5dd36bda06e2e6e86690ed2b79edff558fc8c7d2
Debdip Mondal <69871126+Tatz21@users.noreply.github.com>
2021-02-04 13:31:04 +0530

    cmt

--- Src/loginscreen.js
+++ Src/loginscreen.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { View, Text,StyleSheet,TextInput,TouchableOpacity, Image } from 'react-native';
 
 export default class loginscreen extends React.Component{
+    state= {
+        name:""
+    }
+    continue = () =>{
+        this.props.navigation.navigate("Chat",{name: this.state.name})
+    }
+
+
     render(){
         return(
                <View style={styles.container}>
-                   <Text>LoginScreen</Text>
-               </View>
+                  <View style={styles.circle}/>
+                  <View style={{marginTop:64}}>
+                      <Image 
+                      source={require('../assets/undraw_chat_bot_kli5.png')}
+                      style={{width:240,height:200,marginLeft:0, alignSelf:"stretch"}}
+                      />
+                      </View>
+                    </View>
 
         );
     }
@@ -15,7 +29,15 @@ export default class loginscreen extends React.Component{
 const styles= StyleSheet.create({
     container:{
         flex:1,
-        justifyContent:"center",
-        alignItems:"center"
+       backgroundColor:"#6C61F6"
+    },
+    circle:{
+        width:400,
+        height:400,
+        borderRadius:500 / 2,
+        backgroundColor:"#ffff",
+        position:"absolute",
+        left:-120,
+        top:-20
     }
 });
