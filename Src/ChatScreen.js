d3cfe318f161b8cbd74b81963f57bf04d2f2b0ce
Debdip Mondal <69871126+Tatz21@users.noreply.github.com>
2021-02-06 17:24:26 +0530

    ghgsk

--- Src/ChatScreen.js
+++ Src/ChatScreen.js
@@ -3,14 +3,41 @@ import { Platform, KeyboardAvoidingView, SafeAreaView} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat'
 import Fire from '../Api/Firebase';
 export default class chatscreen extends React.Component{
-    state = 
-    render(){
-        return(
-               <View style={styles.container}>
-                   <Text>ChatScreen</Text>
-               </View>
+    state = {
+        messages: []
+    }
 
+    get user() {
+        return {
+            _id: Fire.uid,
+            name: this.props.navigation.state.params.name
+        }
+    }
+
+
+    componentDidMount() {
+        Fire.get(message=> this.setState(previous =>({
+            messages: GiftedChat.append(previous.message, message)
+        }))
         );
+
+    }
+
+    componentWillUnmount() {
+        Fire.off()
+    }
+
+    render(){
+        const chat = <GiftedChat messages={this.state.messages} onSend={Fire.send} user={this.user}  />
+
+        if (Platform.OS === 'android') {
+            return (
+                <KeyboardAvoidingView style={{flex: 1}} behavior="padding" keyboardVerticalOffset={30} enabled>
+                    {chat}
+                </KeyboardAvoidingView>
+            );
+        }
+    return <SafeAreaView style={{ flex:1}}>{chat}</SafeAreaView>;
     }
 }
 
